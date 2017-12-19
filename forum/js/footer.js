42f09214546b24d0765b823f5da2d827747fe0ec
sivanZhang <445080336@qq.com>
2017-12-19 10:06:27 +0800

    脚本添加, 字体，样式

--- forum/js/footer.js
+++ forum/js/footer.js
@@ -1,10 +1,35 @@
- jQuery(document).ready(function() {
-     jQuery('.code').hide();
-     jQuery('.qqgroup, .wechat').hover(function() {
-         jQuery('.code').show();
-     });
+jQuery(document).ready(function () {
 
-     jQuery(".qqgroup, .wechat").mouseout(function() {
-         jQuery('.code').hide();
-     });
- });
+    /* 下拉框宽度 */
+    jQuery('ul.spinner').width(jQuery('button.spinner_btn').innerWidth());
+
+    /* 输入字数监听 */
+    jQuery("#posted-title").on('keyup input', function (event) {
+        var val = jQuery(this).val();
+        var len = val.length;
+        var count = jQuery(this).siblings('span');
+        if (len == 0) { count.text("0/50"); return; }
+        if (len > 50) {
+            len = 50;
+            jQuery(this).val(val.substring(0, 50));
+        }
+        count.text(len + "/50");
+    });
+
+    /* 二维码hover动画 */
+    jQuery('.wechat').mouseenter(function () {
+        jQuery('.code_weixin').slideToggle();
+    });
+    jQuery('.wechat').mouseleave(function () {
+        jQuery('.code_weixin').slideToggle();
+    });
+
+
+
+    jQuery(".qqgroup").mouseenter(function () {
+        jQuery('.code_qq').slideToggle();
+    });
+    jQuery(".qqgroup").mouseleave(function () {
+        jQuery('.code_qq').slideToggle();
+    });
+});
